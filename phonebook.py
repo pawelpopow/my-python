93ef0652622c04289d9a41c138ca4e07b497061b
ppopow <pawel.popow@gmail.com>
2023-01-25 15:31:12 +0000

    first commit

---
+++ phonebook.py
@@ -0,0 +1,9 @@
+phonebook = {}
+
+phonebook["Jacek"] = 32442123
+phonebook["Ala"] = 23323232
+
+if __name__ == '__main__':
+    print(phonebook)
+    print(phonebook["Jacek"])
+    print(phonebook["Ala"])
