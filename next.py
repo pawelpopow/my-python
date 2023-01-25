d25373be62078e71dfacdb570b789b79395da8fb
ppopow <pawel.popow@gmail.com>
2023-01-25 18:01:22 +0000

    four next

--- next.py
+++ next.py
@@ -1,9 +1,2 @@
 x = 0
 y = 3
-
-if __name__ == '__main__':
-    if x > y:
-        print(f"{x} jest wieksze od {y}")
-
-    if x < y:
-        print(f"{x} jest mniejsze od {y}")
---
+++ next1.py
@@ -0,0 +1,8 @@
+from next import x, y
+
+if __name__ == '__main__':
+    if x > y:
+        print(f"{x} jest wieksze od {y}")
+
+    if x < y:
+        print(f"{x} jest mniejsze od {y}")
