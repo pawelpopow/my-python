c21c6c682d353ed9c153208b4c218e94ae419bd3
ppopow <pawel.popow@gmail.com>
2023-01-26 15:16:29 +0000

    next commit

---
+++ hangman.py
@@ -0,0 +1,15 @@
+def hangman():
+    global count
+    global display
+    global word
+    global already_guessed
+    global play_game
+    limit = 5
+    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
+        print("Invalid Input, Try a letter\n")
+
+
+if __name__ == '__main__':
+    hangman()
---
+++ libraries.py
@@ -0,0 +1,9 @@
+import time
+
+if __name__ == '__main__':
+    print("\nWelcome to Hangman game by DataFlair\n")
+    name = input("Enter your name: ")
+    print("Hello " + name + "! Best of Luck!")
+    time.sleep(2)
+    print("The game is about to start!\n Let's play Hangman!")
+    time.sleep(3)
